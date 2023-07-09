2208014b3a2edd76fa3fc7d83dbfe87902fae916
hprasanth <prasanth.r1247@gmail.com>
2023-07-09 07:46:30 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('hi')
+        else:
+            error_message = 'Invalid username or password.'
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
+
+@login_required
+def hi_view(request):
+    username = request.user.username
+    return render(request, 'hi.html', {'username': username})
+
+def logout_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('hi')
+        else:
+            error_message = 'Invalid username or password.'
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
+
+
